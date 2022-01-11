24441ad979d70fc7e731f70d50212b1fc7d2b506
Yaroslav-ppp <83807204+Yaroslav-ppp@users.noreply.github.com>
2022-01-11 17:11:49 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const photosMarkup = createPhotosMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', photosMarkup);
+
+function createPhotosMarkup(galleryItems) {
+  const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source = "${original}" alt="${description}" width="800" />
+  </a>
+`;
+  });
+
+  return markup.join('');
+}
+galleryContainer.addEventListener('click', onPhotoClick);
+
+function onPhotoClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
 console.log(galleryItems);
